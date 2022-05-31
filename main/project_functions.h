6c5e7a0df4ac598fd5933fa3694a5fce71ef7929
Sergey_Bulanov <gorodtaganrog@gmail.com>
2022-05-31 16:43:21 +0300

    minor 2

--- main/project_functions.h
+++ main/project_functions.h
@@ -1,20 +1,20 @@
 // gpio.c - выдача фрейма
 void gpio_init();    // ранее - gpio_ini()
-void setFrameLow();
-void setFrameHigh();
+void IRAM_ATTR setFrameLow();
+void IRAM_ATTR setFrameHigh();
 
 // rmt.c - выдача зондирующих импульсов
 void rmt_init(channelPulses_t channelPulses);   // ранее - initRmt(...)
-void rmt_run(channelPulses_t channelPulses);    // ранее - runRmt(...)
+void IRAM_ATTR rmt_run(channelPulses_t channelPulses);    // ранее - runRmt(...)
 
 // pwm.c - выдача импульсов для преобразователя напряжения
 void pwm_init();
 
 // dac.c - выдача ВАРУ
 void dac_init();
-void dac_start();
+void IRAM_ATTR dac_start();
 
 // adc.c - чтение блока данных из АЦП в цикле приема
-void adc_run_dma(uint16_t adc1_chan_mask, uint16_t adc2_chan_mask, adc_channel_t *channel, uint8_t channel_num);
+void IRAM_ATTR adc_run_dma(uint16_t adc1_chan_mask, uint16_t adc2_chan_mask, adc_channel_t *channel, uint8_t channel_num);
 // adc_run_dma - ранее называлась - continuous_adc_init(...) и запускалась в start_adc_rmt_dac(...) 
 // ранее start_adc_rmt_dac(...) была главная и единственная функция, запускаемая в главном цикле
